5d52ac8de9539ccfb8dba06683a541859dccd731
SyedAliHadi44 <syedalihadi5918@gmail.com>
2025-11-18 22:23:31 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ export function Header({ currentPage, onNavigate }: HeaderProps) {
             onClick={() => handleNavClick('home')}
           >
             <div className="bg-white p-2 rounded">
-              <img src="/logo.png" alt="Talash-e-Blood" className="h-18 w-auto" />
+              <img src="logo.png" alt="Talash-e-Blood" className="h-17 w-auto" />
             </div>
             <div className="hidden sm:block">
               <h1 className="text-lg">Talash-e-Blood</h1>
